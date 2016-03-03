fab1a7156000ae393abbaedf1a251603e8836cf6
Siedovolosij Serhij <sedovolosiy@gmail.com>
2016-03-03 15:19:50 +0200

    test_main.php

--- views/layouts/test_main.php
+++ views/layouts/test_main.php
@@ -22,17 +22,27 @@ AppAsset::register($this);
 <?php $this->beginBody() ?>
 <center>
 <div id="border">
-    <?= /* header block */ ?>
-    <?= /* left_bar block */ ?>
+
+<!--    /* header block */  -->
+    <?php echo $this->render('header'); ?>
+<!--    end header block-->
+
+<!--    left_bar block-->
+    <?= $this->render('left_bar', ['categories' =>$this->context->getAllCategories()]); ?>
+<!--    end left_bar block-->
+
     <div id="mainarea">
-    <?= /* menus block*/ ?>
+<!--        menu block-->
+        <?= $this->render('menu', ['menus' =>$this->context->getAllMenu()]); ?>
+<!--        end menu block-->
         <div id="main">
 <!--            Content block     -->
             <?= $content ?>
+<!--            end content block-->
         </div>
     </div>
     <div id='bottom'>
-        <?= /* menu bottom block */ ?>
+        <?php echo $this->render('footer'); ?>
     </div>
     <div class="copy"><span class="style1"> Copyright 2010 Название сайта </span>
     </div>
